9b2f89640177bf81182a23cc4712882d6d5f822b
WickJim <wickjim@qq.com>
2020-12-27 19:09:10 +0800

    Study On 2020/12/27

---
+++ AngularDemo/src/app/menber-input/menber-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menber-input',
+  templateUrl: './menber-input.component.html',
+  styleUrls: ['./menber-input.component.scss']
+})
+export class MenberInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
